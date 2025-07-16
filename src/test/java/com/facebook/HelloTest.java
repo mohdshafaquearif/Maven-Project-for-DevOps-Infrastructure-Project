942e2974228f56a739f26a90cb8a2c64191a7510
Mohammad Shafaque Arif <97733931+mohdshafaquearif@users.noreply.github.com>
2025-07-16 17:20:15 +0530

    Create HelloTest.java

---
+++ src/test/java/com/facebook/HelloTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloTest {
+
+    @Test
+    public void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
