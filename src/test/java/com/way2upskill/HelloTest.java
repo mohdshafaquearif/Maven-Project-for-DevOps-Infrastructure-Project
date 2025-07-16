a6d8d81e40da346f439c92e5bfeaf9073db34711
Mohammad Shafaque Arif <97733931+mohdshafaquearif@users.noreply.github.com>
2025-07-16 17:28:38 +0530

    Update HelloTest.java

--- src/test/java/com/way2upskill/HelloTest.java
+++ src/test/java/com/way2upskill/HelloTest.java
@@ -3,10 +3,10 @@ package com.way2upskill;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class HelloTest {
+ class HelloTest {
 
     @Test
-    public void testAddition() {
+    void testAddition() {
         int result = 2 + 3;
         assertEquals(5, result);
     }
